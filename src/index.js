3094d0914cd9122a98c645c9952bf36805f98b45
Dmitry Urukov <urukovdima@gmail.com>
2020-06-06 14:53:37 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const { Telegraf } = require('telegraf');
+require('dotenv-safe').config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
